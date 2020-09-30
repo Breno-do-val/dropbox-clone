54124ccb582b935a6c657a3da5c232876dc57a8f
breno-do-val <brenorvale@gmail.com>
2020-09-30 00:26:58 -0300

    adding events for selecting either single or multiple files

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -350,6 +350,8 @@ class DropBoxController {
               ${this.getFileIconView(file)}
             <div class="name text-center">${file.name}</div>
             `
+        this.initEventsLi(li);
+        
         return li; 
             
     }
@@ -371,6 +373,51 @@ class DropBoxController {
                 this.listFilesE1.appendChild(this.getFileView(data, key));
 
             })
+        })
+    }
+
+    initEventsLi(li) {
+
+        li.addEventListener('click', e => {
+
+            if (e.shiftKey) {
+
+                let firstLi = this.listFilesE1.querySelector('.selected');
+
+                if (firstLi) {
+
+                    let indexStart;
+                    let indexEnd;
+                    let lis =  li.parentElement.childNodes;
+
+                lis.forEach((el, index) => {
+                        if (firstLi === el) indexStart = index;
+                        if (li === el) indexEnd = index;
+                    });
+
+                    let index = [indexStart, indexEnd].sort();
+
+                    lis.forEach((el, i) => {
+
+                        if (i >= index[0] && i <= index[1]) {
+                            el.classList.add('selected');
+                        }
+
+                    });
+
+                    return true;
+                }
+            }
+
+            if (!e.ctrlKey) {
+
+                this.listFilesE1.querySelectorAll('li.selected').forEach(el => {
+                    el.classList.remove('selected');
+                })
+            }
+
+            li.classList.toggle('selected');
+
         })
     }
 };
